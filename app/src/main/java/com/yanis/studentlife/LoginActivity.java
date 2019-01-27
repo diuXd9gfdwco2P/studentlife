bc35de31fd6d42cebf1592a8949404327deaac26
Yanis <yanisakli@yahoo.fr>
2019-01-27 15:55:55 +0100

    LogIn and registration with firebase!!!

---
+++ app/src/main/java/com/yanis/studentlife/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.yanis.studentlife;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText txtEmailAddress;
+    private EditText txtPassword;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txtEmailAddress=(EditText) findViewById(R.id.txtEmailLogin);
+        txtPassword=(EditText) findViewById(R.id.txtPasswordLogin);
+        firebaseAuth=FirebaseAuth.getInstance();
+    }
+
+
+    public void btnUserLogin_Click(View view){
+
+        final ProgressDialog progressDialog=ProgressDialog.show(LoginActivity.this,"Please wait ...!!!","Processing ...",true);
+        firebaseAuth.signInWithEmailAndPassword(txtEmailAddress.getText().toString(),txtPassword.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login successful",Toast.LENGTH_LONG).show();
+                            Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                            startActivity(i);
+
+                        }else{
+                            Log.e("ERROR",task.getException().toString());
+                            Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
--- app/src/main/java/com/yanis/studentlife/MainActivity.java
+++ app/src/main/java/com/yanis/studentlife/MainActivity.java
@@ -1,7 +1,12 @@
 package com.yanis.studentlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnRegistration_click(View view){
+        Intent i =new Intent(MainActivity.this,RegistrationActivity.class);
+        startActivity(i);
+
+    }
+
+    public void btnLogin_click(View view){
+        Intent i =new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(i);
     }
 }
