c6dd1230d47d29c869b237249cd0aefcb0ed2aba
Yanis <yanisakli@yahoo.fr>
2019-02-03 21:40:38 +0100

    add class offer

---
+++ app/src/main/java/com/yanis/studentlife/offer.java
@@ -0,0 +1,36 @@
+package com.yanis.studentlife;
+
+public class offer {
+    private String id;
+    private String name;
+    private String place;
+    private String date;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public offer(String name, String place, String date, String phone) {
+        this.name = name;
+        this.place = place;
+        this.date = date;
+        this.phone = phone;
+    }
+}
