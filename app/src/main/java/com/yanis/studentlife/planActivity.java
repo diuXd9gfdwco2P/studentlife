e1d4cb3cdae1f3fdd49b8350784bb6b510b3e8d3
Yanis <yanisakli@yahoo.fr>
2019-02-07 21:43:23 +0100

    Read and lister all the plans from the database

--- app/src/main/java/com/yanis/studentlife/planActivity.java
+++ app/src/main/java/com/yanis/studentlife/planActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
@@ -14,26 +16,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class planActivity extends SharedMainActivity {
 
     FirebaseFirestore db=FirebaseFirestore.getInstance();
+    RecyclerView recyclerView;
+    planAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+
+        recyclerView=(RecyclerView)findViewById(R.id.recyclerViewPlan);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         db.collection("plan")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-
+                            List<plan> list=new ArrayList<>();
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("READ_Plan", document.getId() + " => " + document.getData());
+                                list.add(new plan(document.getString("name"),document.getString("address"),document.getString("date"),document.getString("phone")));
                             }
+                            adapter=new planAdapter(planActivity.this,list);
+                            recyclerView.setAdapter(adapter);
                         } else {
-                            Log.w("READ_Plan", "Error getting documents.", task.getException());
+                            Log.w("READ_EVENT", "Error getting documents.", task.getException());
                         }
                     }
                 });
---
+++ app/src/main/java/com/yanis/studentlife/planAdapter.java
@@ -0,0 +1,55 @@
+package com.yanis.studentlife;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class planAdapter extends RecyclerView.Adapter<planAdapter.planViewHolder> {
+    private Context ctx;
+    private List<plan> planList;
+
+    public planAdapter(Context ctx, List<plan> planList) {
+        this.ctx = ctx;
+        this.planList = planList;
+    }
+
+    @NonNull
+    @Override
+    public planAdapter.planViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater=LayoutInflater.from(ctx);
+        View view=inflater.inflate(R.layout.card,null);
+        return new planViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull planAdapter.planViewHolder planViewHolder, int i) {
+        plan plan=planList.get(i);
+
+        planViewHolder.textViewName.setText(plan.getName());
+        planViewHolder.textViewPlace.setText(plan.getPlace());
+        planViewHolder.textViewDate.setText(plan.getDate());
+        planViewHolder.textViewPhone.setText(plan.getPhone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return planList.size();
+    }
+
+    public class planViewHolder extends RecyclerView.ViewHolder{
+        TextView textViewName,textViewPlace,textViewDate,textViewPhone;
+        public planViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewName=itemView.findViewById(R.id.txtNameEvent);
+            textViewPlace=itemView.findViewById(R.id.txtPlaceEvent);
+            textViewDate=itemView.findViewById(R.id.txtDateEvent);
+            textViewPhone=itemView.findViewById(R.id.txtPhoneEvent);
+        }
+    }
+}
